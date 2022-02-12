ffba2e6fbaaaacda1ab477dd6cfdaff44bfb3eb4
Johannes Löthberg <johannes@kyriasis.com>
2022-02-13 00:45:48 +0100

    main: Set clock to 72 MHz
    Signed-off-by: Johannes Löthberg <johannes@kyriasis.com>

--- src/main.zig
+++ src/main.zig
@@ -14,7 +14,33 @@ const startup_device = @field(startup.Device, @tagName(build_options.device));
 const VectorTable = startup.VectorTable;
 export const vector_table linksection(".vector_table") = VectorTable(main, startup_device).init(.{});
 
+fn set_clock() void {
+    // Set PLLMUL to x9, don't divide HSE before PLL, pass HSE into PLL, and APB1 to divide by 2.
+    regs.RCC.CFGR.modify(.{ .PLLMUL = 0b0111, .PLLXTPRE = 0b0, .PLLSRC = 0b1, .PPRE1 = 0b100 });
+    regs.RCC.CR.modify(.{ .PLLON = 0b1, .HSEON = 0b1 });
+
+    // Wait until both HSE and PLL is ready.
+    var cr = regs.RCC.CR.read();
+    while (cr.HSERDY != 0b1 or cr.PLLRDY != 0b1) {
+        cr = regs.RCC.CR.read();
+    }
+
+    // Set flash latency to two wait states.  If this is too slow for the set
+    // SYSCLK speed the processor will trap as it couldn't read the
+    // instructions in time.
+    // 000 Zero wait state, if 0      < SYSCLK <= 24 MHz
+    // 001 One wait state,  if 24 MHz < SYSCLK <= 48 MHz
+    // 010 Two wait states, if 48 MHz < SYSCLK <= 72 MHz
+    regs.FLASH.ACR.modify(.{ .LATENCY = 0b010 });
+
+    // Set SYSCLK source to PLL and wait until we've switched over.
+    regs.RCC.CFGR.modify(.{ .SW = 0b10 });
+    while (regs.RCC.CFGR.read().SWS != 0b10) {}
+}
+
 pub fn main() void {
+    set_clock();
+
     regs.RCC.APB2ENR.modify(.{ .IOPCEN = 1 });
     regs.RCC.APB2RSTR.modify(.{ .IOPCRST = 1 });
     regs.RCC.APB2RSTR.modify(.{ .IOPCRST = 0 });
